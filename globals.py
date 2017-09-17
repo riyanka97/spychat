27dbb33e5fdc27fbd10b42032a71a6971c2b9e20
riyanka garg <you@example.com>
2017-09-17 19:07:52 +0530

    project done

---
+++ globals.py
@@ -0,0 +1,26 @@
+from spy_details import Spy
+from datetime import datetime
+
+
+status = ['My name is Bond, James Bond', ' I C Python', 'Its gonna blow']
+
+# Spy object
+spy = Spy("Dev", "Mr. ", 31, 2.1)
+
+# Friends
+friend_one = Spy('vim', 'Mr.', 21, 4)
+friend_two = Spy('Manisha', 'Ms.', 21, 5)
+friend_three = Spy('Rai', 'Dr.', 50, 4.5)
+
+friends = [friend_one, friend_two, friend_three]
+
+# Class to store Chat messages
+
+
+class ChatMessage:
+
+  def __init__(self, message, sent_by_me):
+    self.message = message
+    self.time = datetime.now()
+    self.sent_by_me = sent_by_me
+
